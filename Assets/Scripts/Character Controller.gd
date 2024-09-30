8e7376c4b1b098071f155a6bf94ea00fcb02e0d3
yadavvishal1 <v2s2studios@gmail.com>
2024-09-30 15:33:23 +0530

    Chracater Rotation

--- Assets/Scripts/Character Controller.gd
+++ Assets/Scripts/Character Controller.gd
@@ -1,27 +1,33 @@
 extends Node3D
 
 @export var move_speed: float = 6
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+@export var rot_speed: float = 3
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var inputvector: Vector3 = Vector3(0, 0, 0)
+	var input_vector: Vector3 = Vector3(0, 0, 0)
+	var forward: Vector3 = transform.basis.z
 	
 	if Input.is_action_pressed("Up"):
-		inputvector.z = -1;
+		input_vector.z = -1
 	if Input.is_action_pressed("Down"):
-		inputvector.z = +1;
+		input_vector.z = +1
 	if Input.is_action_pressed("Left"):
-		inputvector.x = -1
+		input_vector.x = -1
 	if Input.is_action_pressed("Right"):
-		inputvector.x = +1
+		input_vector.x = +1
 	
-	inputvector = inputvector.normalized()
+	input_vector = input_vector.normalized()
 	
-	transform.origin += Vector3(inputvector) * move_speed * delta
+	var move_dir:Vector3 = Vector3(input_vector.x, 0, input_vector.z)
+	transform.origin += Vector3(move_dir) * move_speed * delta
+	rotation.y = lerp_angle(rotation.y, atan2(move_dir.x, move_dir.z), delta * rot_speed)
+
+	
+
+	
+	
+	
 	
 	
 	
