54ae599bb5f022737133112a909cb469cf2b6242
yadavvishal1 <v2s2studios@gmail.com>
2024-11-04 18:30:45 +0530

    Kitchen Object, Scriptable Objects
    I Explored Custom Resources in Godot which is Equivalent to Scriptable Objects in Unity

--- Scripts/Counter.gd
+++ Scripts/Counter.gd
@@ -1,22 +1,13 @@
 class_name Counter extends StaticBody3D
 
 @export var selected_object: Node3D
-@export var kitchen_object: KicthenObjectResource = null
-@export var counter_top_point: Node3D
 
-var placed_object = null
 
 func interact():
+	# Logic to clear the counter
 	print("Interact Called")
-	if placed_object == null:
-		var object = kitchen_object.model.instantiate()
-		add_child(object)
-		object.position = counter_top_point.position
-		if object.has_method("get_kitchen_object_resource"):
-			object.kitchen_object_resource = kitchen_object
-			placed_object = kitchen_object
-			print("Object placed on counter:", object.kitchen_object_resource.object_name)
-		print(name)
+	print(name)
+	# Add your clearing logic here
 
 func select():
 	selected_object.visible = true
